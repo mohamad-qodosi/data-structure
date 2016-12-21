ded0d84b6e3028a168cdf72941813f694077a8cd
Mohamad Qodosi <mohamadqodosi@yahoo.com>
2016-12-22 01:28:39 +0330

    Add stable and complete Stack class

--- Stack.java
+++ Stack.java
@@ -1,20 +1,42 @@
 public class Stack {
 	
 	int data[];
+	int back, size;
 
 	public Stack(int size) {
-		// TO DO : define data;
+		data = new int[size];
+		back = 0;
+		this.size = size;
 	}
 
 	public void push(int d) {
-		// TO DO : add d into data
+		if (back == size) {
+			extend();
+		}
+		data[back] = d;
+		back++;
 	}
 
 	public int pop() {
-		// TO DO : delete data.last and return it
+		if (back != 0) {
+			back--;
+			return data[back];
+		}
+		else {
+			return -1;
+		}
 	}
 
 	private void extend() {
-		// TO DO : make data bigger
+		// we can first copy data to an other array, then re-make data array bigger and copy data to it back
+		// this will need two loops.
+		// its better to make a new list , bigger than data , copy data to it and set data equal to new list
+
+		int temp[] = new int[size + 1];
+		for(int i = 0 ;i < size;i++) {
+			temp[i] = data[i];
+		}
+		size++;
+		data = temp;
 	}
 }
